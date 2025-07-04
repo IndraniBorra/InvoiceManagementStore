68336f0c71587f07c580094704242588f73b20cd
IndraniBorra <indraniborra999@gmail.com>
2025-07-04 16:57:43 -0500

    deleting old folder names: files

--- React/invoicemanagement-app/src/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'  // connection libray were we are able to connect react application to out fastapi application
-
-const api = axios.create({
-    baseURL: 'http://192.168.1.217:8000', // base URL of our fastapi application
-})
-
-export default api;
