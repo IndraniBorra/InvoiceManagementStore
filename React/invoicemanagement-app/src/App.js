fea4b05df7e0b8b5ef742a393657f3ace4eb0b37
IndraniBorra <indraniborra999@gmail.com>
2025-07-04 10:42:09 -0500

    Added invoicemanagement-app as part of main repo

---
+++ React/invoicemanagement-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import InvoiceForm from './components/InvoiceForm';
+// import InvoiceList from './components/InvoiceList';
+import InvoicePage from './components/InvoicePage';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Invoice Management App</h1>
+      <InvoicePage />
+      {/* <InvoiceList /> */}
+    </div>
+  );
+}
+
+export default App;
